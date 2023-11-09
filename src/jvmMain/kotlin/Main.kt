d2f99f75c48e13454c0b9414ce9d8c1c5eb2318d
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-11-09 23:15:51 +0200

    Big image

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,16 +1,22 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.platform.Font
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -20,32 +26,95 @@ import java.io.File
 @Composable
 @Preview
 fun App() {
-    val file = remember { File("resources\\UI-QuestGreeting-TopLeft.png") }
-    val font = remember {
-        FontFamily(
-            Font(
-                resource = "font\\MORPHEUS.TTF",
-                style = FontStyle.Normal
-            )
+    val topLeftImage = remember { File("resources\\UI-QuestGreeting-TopLeft.png") }
+    val topRightImage = remember { File("resources\\UI-QuestGreeting-TopRight.png") }
+    val bottomLeftImage = remember { File("resources\\UI-QuestGreeting-BotLeft.png") }
+    val bottomRightImage = remember { File("resources\\UI-QuestGreeting-BotRight.png") }
+
+    val bookIcon = remember { File("resources\\UI-QuestLog-BookIcon.png") }
+
+    val titleFont = remember {
+        TextStyle(
+            fontFamily = FontFamily(
+                Font(
+                    resource = "font\\MORPHEUS.TTF",
+                    style = FontStyle.Normal
+                )
+            ),
+            fontSize = 18.sp,
+        )
+    }
+
+    val descriptionStyle = remember {
+        TextStyle(
+            fontFamily = FontFamily(
+                Font(
+                    resource = "font\\FRIZQT__.TTF",
+                    style = FontStyle.Normal
+                )
+            ),
+            fontSize = 13.sp,
         )
     }
 
     MaterialTheme {
-        Image(
-            bitmap = Image.makeFromEncoded(file.readBytes()).toComposeImageBitmap(),
-            ""
-        )
-        Text(
-            modifier = Modifier.graphicsLayer {
-                translationY = 82f
-                translationX = 31f
-            },
-            style = TextStyle(
-                fontFamily = font,
-                fontSize = 18.sp,
-            ),
-            text = "Wool Would Work"
-        )
+        Row {
+            Column {
+                Image(
+                    bitmap = remember { Image.makeFromEncoded(topLeftImage.readBytes()).toComposeImageBitmap() },
+                    ""
+                )
+                Image(
+                    bitmap = remember { Image.makeFromEncoded(bottomLeftImage.readBytes()).toComposeImageBitmap() },
+                    ""
+                )
+            }
+            Column {
+                Image(
+                    bitmap = remember { Image.makeFromEncoded(topRightImage.readBytes()).toComposeImageBitmap() },
+                    ""
+                )
+                Image(
+                    bitmap = remember { Image.makeFromEncoded(bottomRightImage.readBytes()).toComposeImageBitmap() },
+                    ""
+                )
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .width(200.dp)
+                .graphicsLayer {
+                    translationY = 82f
+                    translationX = 31f
+                },
+        ) {
+            TextField(
+                textStyle = titleFont,
+                value = "Wool Would Work",
+                onValueChange = {},
+                maxLines = 1,
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedLabelColor = Color.Transparent,
+                    unfocusedLabelColor = Color.Transparent
+                )
+            )
+            TextField(
+                textStyle = descriptionStyle,
+                value = "Gather 20 bundles of wool off the sheep in Elwynn Forest and bring them back to Julie Osworth.",
+                onValueChange = {},
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedLabelColor = Color.Transparent,
+                    unfocusedLabelColor = Color.Transparent
+                )
+            )
+        }
     }
 }
 
