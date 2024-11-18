ab217f0234e4a20902920fd7111d3395d878b15a
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2024-11-18 14:54:59 +0200

    Create setSummary for QuestContainer

--- src/jvmMain/kotlin/QuestContainer.kt
+++ src/jvmMain/kotlin/QuestContainer.kt
@@ -1,6 +1,26 @@
 data class QuestContainer(
     override val id: String,
     val title: String,
+    val path: String,
     val expanded: Boolean,
     val questList: List<Quest>,
-): Identifiable
+) : Identifiable
+
+fun QuestContainer.setSummary(
+    questId: String,
+    summary: String
+): QuestContainer {
+    val quest = questList.first { it.id == questId }
+    val newQuestList = questList.set(quest, { quest.copy(summary = summary) })
+    return this.copy(questList = newQuestList)
+}
+
+fun List<QuestContainer>.setSummary(
+    containerId: String,
+    questId: String,
+    summary: String
+): List<QuestContainer> {
+    val questContainer = this.first { it.id == containerId }
+    val newQuestContainer = questContainer.setSummary(questId, summary)
+    return this.set(questContainer, { newQuestContainer })
+}
